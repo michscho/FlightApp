b28b5bd64b9e69f1858372fe689ea53e0e8f3e43
WORKBOOK\Hannes Simon <hannes.simon@tum.de>
2018-05-16 10:53:30 +0200

    really basic UI

---
+++ BookAndFlight/src/UI/Window.java
@@ -0,0 +1,54 @@
+package UI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+
+public class Window extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Window frame = new Window();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Window() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 11, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		table = new JTable();
+		table.setBounds(51, 98, 240, 20);
+		contentPane.add(table);
+	}
+}
