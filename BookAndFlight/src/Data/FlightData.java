c89801b75cd760b915960388eb55b34f0810276a
Michael Schott <schottmichaelsw@gmail.com>
2018-05-16 13:55:38 +0200

    Window added; Search function implemented

--- BookAndFlight/src/Data/FlightData.java
+++ BookAndFlight/src/Data/FlightData.java
@@ -9,10 +9,9 @@ public class FlightData {
     public static FlightModel initiateData(){
         FlightModel model = new FlightModel();
 
-        model.addFlight( new Flight( "MünchenBerlin",  "München", "Berlin", 2, 50, false));
+        model.addFlight( new Flight( "M\u00FCnchenBerlin",  "M\u00FCnchen", "Berlin", 2, 50, false));
         model.addFlight( new Flight( "FrankfurtUSA",  "Frankfurt", "USA", 10, 550, false));
         model.addFlight( new Flight( "FrankfurtAustralia",  "Frankfurt", "Australia", 12, 750, true));
-
 
         return model;
     }
