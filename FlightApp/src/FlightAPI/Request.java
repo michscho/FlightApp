d7ca90d4cb19a9666b58d938ef2a416808e5c467
Michael Schott <schottmichaelsw@gmail.com>
2018-06-10 00:37:27 +0200

    developer Modus added

--- FlightApp/src/FlightAPI/Request.java
+++ FlightApp/src/FlightAPI/Request.java
@@ -1,9 +1,19 @@
 package FlightAPI;
 
+import Main.Main;
 import javafx.scene.control.Alert;
+import org.w3c.dom.Document;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -13,36 +23,58 @@ public class Request {
     private static final String token = "bwhj6dfr93b3cyb4tctdbjzm";
 
      public static String request(String request) throws Exception {
-            String url = request;
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("Authorization", "Bearer " + token + " ");
-            con.setRequestProperty("Accept","application/xml");
-            int responseCode = con.getResponseCode();
-            System.out.println("\nSending 'GET' request to URL : " + url);
-            System.out.println("Response Code : " + responseCode);
+         // In developerModues you don't send any request to Lufthansa
+         if (Main.developerModus){
+             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+             DocumentBuilder db = dbf.newDocumentBuilder();
+             Document doc = db.parse("file:///" + System.getProperty("user.dir") + "\\resources\\data\\sampleResponse.xml");
+             System.out.println(toXmlString(doc));
+             return toXmlString(doc);
+         } else {
+             String url = request;
+             URL obj = new URL(url);
+             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+             con.setRequestMethod("GET");
+             con.setRequestProperty("Authorization", "Bearer " + token + " ");
+             con.setRequestProperty("Accept", "application/xml");
+             int responseCode = con.getResponseCode();
+             System.out.println("\nSending 'GET' request to URL : " + url);
+             System.out.println("Response Code : " + responseCode);
 
-            if (responseCode != HttpURLConnection.HTTP_OK){
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("Your request failed");
-                alert.setContentText("Error code: " + responseCode);
-                alert.showAndWait();
-            }
+             if (responseCode != HttpURLConnection.HTTP_OK) {
+                 Alert alert = new Alert(Alert.AlertType.ERROR);
+                 alert.setTitle("Error");
+                 alert.setHeaderText("Your request failed");
+                 alert.setContentText("Error code: " + responseCode);
+                 alert.showAndWait();
+             }
 
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            System.out.println(response.toString());
-            return response.toString();
+             BufferedReader in = new BufferedReader(
+                     new InputStreamReader(con.getInputStream()));
+             String inputLine;
+             StringBuffer response = new StringBuffer();
+             while ((inputLine = in.readLine()) != null) {
+                 response.append(inputLine);
+             }
+             in.close();
+             System.out.println(response.toString());
+             return response.toString();
+         }
 
         }
+
+    private static String toXmlString(Document document) throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(document);
+        StringWriter strWriter = new StringWriter();
+        StreamResult result = new StreamResult(strWriter);
+
+        transformer.transform(source, result);
+
+        return strWriter.getBuffer().toString();
+
+    }
 
 
 
--- FlightApp/src/FlightInformation/FlightInformationController.java
+++ FlightApp/src/FlightInformation/FlightInformationController.java
@@ -2,18 +2,24 @@ package FlightInformation;
 
 import Data.Classes.Flight;
 import Data.Converter.IANACodeConverter;
+import Data.Database.DataManager;
 import Main.Main;
-import Main.MainController;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.text.Text;
 
-import javax.swing.text.TableView;
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
+import java.sql.DatabaseMetaData;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class FlightInformationController implements Initializable {
@@ -32,16 +38,51 @@ public class FlightInformationController implements Initializable {
     private Text arrival;
 
     @FXML
+    private TreeView treeView;
+
+    @FXML
     private Image wheather_today;
     @FXML
     private Image wheather_tomorrow;
     @FXML
     private Image wheather_future;
 
-    public FlightInformationController(){
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private Button requestButton;
+    @FXML
+    private Button rateButton;
+    @FXML
+    private Button mapButton;
+
+    @FXML
+    private Pagination page;
+
+    @FXML
+    private Text text;
+
+    public FlightInformationController() {
     }
 
-    public void setText(ObservableList<Flight> observableList) throws Exception{
+    public void menuAboutFlightAppClicked() {
+        // TODO Small Frame with some Information
+    }
+
+    public void menuSettingsClicked() {
+        // TODO Setting Frame, for example: Toggle: Search with Case Sensitive
+    }
+
+    public void menuUserClicked() {
+        // TODO Display UserInformation
+    }
+
+    public void menuQuitClicked() {
+        Platform.exit();
+    }
+
+
+    public void setText(ObservableList<Flight> observableList) throws Exception {
         flightNumber.setText(observableList.get(0).getFlightNumberC());
         departureTime.setText(observableList.get(0).getStartTimeC());
         arrivalTime.setText(observableList.get(0).getEndTimeC());
@@ -54,9 +95,33 @@ public class FlightInformationController implements Initializable {
         Main.showMainView();
     }
 
+    public void setTreeView() {
+        if (DataManager.loadFlightData() == null && Main.developerModus == false) {
+            treeView.setVisible(false);
+            mapButton.setVisible(false);
+            rateButton.setVisible(false);
+            requestButton.setVisible(false);
+            cancelButton.setVisible(false);
+            page.setVisible(false);
+        } else {
+            text.setVisible(false);
+        }
+
+
+        TreeViewHelper helper = new TreeViewHelper();
+        ArrayList<TreeItem> data = helper.getData();
+
+        TreeItem treeItemRoot = new TreeItem("Flight:");
+        treeItemRoot.getChildren().addAll(data);
+        treeView.setRoot(treeItemRoot);
+
+    }
+
+
     // TODO: UserData: @Michael
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        setTreeView();
     }
 }
