b7287a3590c35fa1be7de168e2ed0c0dc228719e
Michael Schott <schottmichaelsw@gmail.com>
2018-06-09 15:50:43 +0200

    IANA to City bug solved

--- FlightApp/src/Data/Converter/IANACodeConverter.java
+++ FlightApp/src/Data/Converter/IANACodeConverter.java
@@ -1,17 +1,18 @@
 package Data.Converter;
 
-
 import Util.StringUtil;
 import com.Ostermiller.util.CSVParser;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class IANACodeConverter {
 
+    /**
+     * @param cityName
+     * @return
+     * @throws IOException
+     */
     public static String getIANACode(String cityName) throws IOException {
         CSVParser csvParser = new CSVParser( new FileInputStream(System.getProperty("user.dir") + "/resources/data/AirportToIANACode.csv") );
         for ( String t; (t = csvParser.nextValue()) != null; ) {
@@ -28,24 +29,34 @@ public class IANACodeConverter {
 
         }
         csvParser.close();
-        return "";
+        return "invalid City Name";
     }
 
+    /**
+     *
+     * @param IANACode
+     * @return
+     * @throws IOException
+     */
     public static String IANAToCity(String IANACode) throws  IOException{
         CSVParser csvParser = new CSVParser( new FileInputStream(System.getProperty("user.dir") + "/resources/data/AirportToIANACode.csv") );
-        CSVParser csvParserTmp = new CSVParser( new FileInputStream(System.getProperty("user.dir") + "/resources/data/AirportToIANACode.csv") );
-        String tmp = "";
-        for ( String t; (t = csvParser.nextValue()) != null; ) {
-            if (t.equals(IANACode)){
-                return tmp;
+        for (int i = 0; csvParser.getLastLineNumber() < 9020; i++) {
+            String t1 = csvParser.nextValue();
+            String t2 = csvParser.nextValue();
+            if (IANACode.equals(t2)){
+                return t1;
             }
-            tmp = csvParserTmp.nextValue();
         }
         csvParser.close();
-        return "";
+        return "invalid IANACode";
     }
 
 
+    /**
+     *
+     * @return String with all IANA Codes and city names
+     * @throws IOException
+     */
     public static String[] getAllAttributes() throws IOException {
         CSVParser csvParser = new CSVParser( new FileInputStream(System.getProperty("user.dir") + "/resources/data/AirportToIANACode.csv") );
         String[][] data = csvParser.getAllValues();
