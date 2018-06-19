8ba4132884989941111eb1245b8e8d312d41f403
Michael Schott <ga48xop@mytum.de>
2018-06-19 15:12:18 +0200

    changed test-structure

---
+++ FlightApp/src/test/main/java/Data/Converter/IANACodeConverterTest.java
@@ -0,0 +1,42 @@
+package Data.Converter;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IANACodeConverterTest {
+
+
+
+        @Test
+        public void testGetIANACodeBasic() throws IOException {
+            assertEquals("FRA", IANACodeConverter.getIANACode("Frankfurt")) ;
+            assertEquals("IST", IANACodeConverter.getIANACode("Istanbul")) ;
+            assertEquals("SOF", IANACodeConverter.getIANACode("Sofia")) ;
+
+        }
+
+        @Test
+        public void testGetIANACodeSpecial() throws IOException {
+            String randomString = "sidjx";
+            for (int i = 0; i < 10; i++) {
+                assertEquals(IANACodeConverter.Error.INVALID_CITY_NAME.toString(), IANACodeConverter.getIANACode(randomString));
+                randomString += 3;
+            }
+            assertEquals(IANACodeConverter.Error.NULL_OR_EMPTY.toString(), IANACodeConverter.getIANACode(""));
+            assertEquals(IANACodeConverter.Error.NULL_OR_EMPTY.toString(), IANACodeConverter.getIANACode(null));
+        }
+
+//        @Test
+//        public void testGetIANAtoCity() throws IOException {
+//            assertEquals("Frankfurt", IANACodeConverter.IANAToCity("FRA")) ;
+//            assertEquals("Istanbul", IANACodeConverter.IANAToCity("IST")) ;
+//            assertEquals("Sofia", IANACodeConverter.IANAToCity("SOF")) ;
+//
+//        }
+
+    }
+
