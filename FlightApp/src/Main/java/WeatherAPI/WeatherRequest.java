3284bf20db5fb6a529560aa2045e586d2209a18e
Michael Schott <schottmichaelsw@gmail.com>
2018-06-23 23:51:45 +0200

    Added temperature overview in FlightInformationFrame

--- FlightApp/src/Main/java/WeatherAPI/WeatherRequest.java
+++ FlightApp/src/Main/java/WeatherAPI/WeatherRequest.java
@@ -1,12 +1,11 @@
-package WeatherAPI; // TODO: show current weather and weather on arrival
+package WeatherAPI;
 
-import javafx.scene.image.Image;
 import org.json.JSONObject;
 
 import java.net.URL;
 import java.util.Scanner;
 
-public class WeatherRequest {
+public class WeatherRequest { // TODO: Array zurückgeben mit lediglich einem Request pro Aufruf
 
     private static final String APIKey = "39b1bc038434e7c0b3db7e4b96a6afb9";
     public String city;
@@ -16,20 +15,13 @@ public class WeatherRequest {
     }
 
     public static void main(String[] args) {
-        String munich = "3220838";
-        String berlin = "2950158";
-
         WeatherRequest wr = new WeatherRequest("München");
-        String tmp = "";
         try {
-            System.out.println(wr.currentWeather());
-            tmp = wr.currentWeather();
+            System.out.println(wr.tempForecast(16));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-//        Image image = new Image("http://openweathermap.org/img/w/" + tmp + ".png");
-//        System.out.println("Image URL: " + image.getUrl());
     }
 
     public String urlBuilder (String url) throws Exception {
@@ -61,6 +53,24 @@ public class WeatherRequest {
 
         JSONObject res = obj.getJSONArray("list").getJSONObject(index); // 1 Day: index 8; 2 Days: index 16
         return res.getJSONArray("weather").getJSONObject(0).getString("icon");
+    }
+
+    public int currentTemp () throws Exception {
+        String url = urlBuilder("https://api.openweathermap.org/data/2.5/weather?q=");
+        JSONObject obj = new JSONObject(url);
+
+        JSONObject temp = obj.getJSONObject("main");
+        int kel = temp.getInt("temp");
+        return kel - 273;
+    }
+
+    public int tempForecast (int index) throws Exception {
+        String url = urlBuilder("https://api.openweathermap.org/data/2.5/forecast?q=");
+        JSONObject obj = new JSONObject(url);
+
+        JSONObject res = obj.getJSONArray("list").getJSONObject(index); // 1 Day: index 8; 2 Days: index 16
+        int kel = res.getJSONObject("main").getInt("temp");
+        return kel - 273;
     }
 
 }
