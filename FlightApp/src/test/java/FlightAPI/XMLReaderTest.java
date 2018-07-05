698045e75aaf3333354e02e09218045181c1d31d
Michael Schott <ga48xop@mytum.de>
2018-07-05 22:40:54 +0200

    automatic tokken generation and code refactoring

--- FlightApp/src/test/java/FlightAPI/XMLReaderTest.java
+++ FlightApp/src/test/java/FlightAPI/XMLReaderTest.java
@@ -15,11 +15,11 @@ public class XMLReaderTest {
         Main.developerModus = true;
         List<Flight> list = XMLReader.readInput(Request.request(""));
         Flight flight = list.get(0);
-        assertEquals(flight.getEndAirportC(),"TXL");
-        assertEquals(flight.getEndTimeC(),"2018-06-10T08:05");
+        assertEquals(flight.getEndAirportC(),"Berlin");
+        assertEquals(flight.getEndTimeC(),"10.06 at 08:05");
         assertEquals(flight.getFlightNumberC(),"2030");
-        assertEquals(flight.getStartAirportC(),"MUC");
-        assertEquals(flight.getStartTimeC(),"2018-06-10T07:00");
+        assertEquals(flight.getStartAirportC(),"Munich");
+        assertEquals(flight.getStartTimeC(),"10.06 at 07:00");
         assertEquals(flight.getAirlineC(),"LH");
         assertEquals(flight.getAirplaneTypeC(),"na");
 
