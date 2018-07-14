e34523aef9c38e2bdb551224292d45a347d7f478
Michael Schott <ga48xop@mytum.de>
2018-07-14 14:22:07 +0200

    fixed UTF8 Bug

--- FlightApp/src/main/java/Data/Converter/IANACodeConverter.java
+++ FlightApp/src/main/java/Data/Converter/IANACodeConverter.java
@@ -5,6 +5,8 @@ import com.Ostermiller.util.CSVParser;
 
 import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IANACodeConverter {
 
@@ -23,7 +25,7 @@ public class IANACodeConverter {
 
         try {
 
-            br = new BufferedReader(new InputStreamReader(airportCSV.openStream()));
+            br = new BufferedReader(new InputStreamReader(airportCSV.openStream(),"UTF8"));
             while ((line = br.readLine()) != null) {
 
                 String[] country = line.split(cvsSplitBy);
@@ -69,7 +71,7 @@ public class IANACodeConverter {
 
         try {
 
-            br = new BufferedReader(new InputStreamReader(airportCSV.openStream()));
+            br = new BufferedReader(new InputStreamReader(airportCSV.openStream(),"UTF8"));
             while ((line = br.readLine()) != null) {
 
                 String[] country = line.split(cvsSplitBy);
@@ -99,24 +101,42 @@ public class IANACodeConverter {
     }
 
 
-    public static CSVParser getCSVParser(){
-        try {
-            return new CSVParser(airportCSV.openStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
     /**
      * @return String with all IANA Codes and city names
      * @throws IOException
      */
     public static String[] getAllAttributes() throws IOException {
-        CSVParser csvParser = getCSVParser();
-        String[][] data = csvParser.getAllValues();
-        return StringUtil.flatten(data);
+        List<String> list = new ArrayList<>();
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
 
+        try {
+
+            br = new BufferedReader(new InputStreamReader(airportCSV.openStream(),"UTF8"));
+            while ((line = br.readLine()) != null) {
+
+                String[] country = line.split(cvsSplitBy);
+                for (int i = 0; i < country.length; i++) {
+                    list.add(country[i]);
+                    }
+                }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return list.toArray(new String[0]);
     }
 
 
